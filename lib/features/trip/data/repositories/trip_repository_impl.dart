10c0bc2d6db2743426f22a666a91540431230a6e
MoaazAtik <moaaz.atik@gmail.com>
2025-06-02 18:51:40 +0300

    feat: Implement TripRepositoryImpl for local data source
    This commit introduces `TripRepositoryImpl`, an implementation of the `TripRepository` interface.
    
    Key responsibilities:
    - Manages trip data by interacting with a `TripLocalDataSource`.
    - Converts between `Trip` (domain entity) and `TripModel` (data layer model) for persistence.
    
    Methods:
    - `addTrip(Trip trip)`: Converts `Trip` to `TripModel` and adds it via the local data source.
    - `deleteTrip(int index)`: Deletes a trip from the local data source by its index.
    - `getTrips()`: Retrieves `TripModel` objects from the local data source and converts them to a list of `Trip` entities.

---
+++ lib/features/trip/data/repositories/trip_repository_impl.dart
@@ -0,0 +1,31 @@
+/* If I was using multiple data sources like a remote API and a local datasource,
+ this would be where I implement the logic of mixing data of both of them. */
+
+import '../../domain/entities/trip.dart';
+import '../../domain/repositories/trip_repository.dart';
+import '../datasources/trip_local_datasource.dart';
+import '../models/trip_model.dart';
+
+class TripRepositoryImpl extends TripRepository {
+  final TripLocalDataSource localDataSource;
+
+  TripRepositoryImpl(this.localDataSource);
+
+  @override
+  Future<void> addTrip(Trip trip) async {
+    final tripModel = TripModel.fromEntity(trip);
+    localDataSource.addTrip(tripModel);
+  }
+
+  @override
+  Future<void> deleteTrip(int index) async {
+    localDataSource.deleteTrip(index);
+  }
+
+  @override
+  Future<List<Trip>> getTrips() async {
+    final tripModels = localDataSource.getTrips();
+    List<Trip> res = tripModels.map((model) => model.toEntity()).toList();
+    return res;
+  }
+}
