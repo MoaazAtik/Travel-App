899ab3f4f241115495ecc711b854e4db4265f808
MoaazAtik <moaaz.atik@gmail.com>
2025-06-02 16:57:28 +0300

    feat: Define TripRepository interface (abstract class)
    This commit introduces the `TripRepository` abstract class, which defines the contract for managing trip data.
    
    It includes methods for:
    - Retrieving all trips (`getTrips`)
    - Adding a new trip (`addTrip`)
    - Deleting a trip by its index (`deleteTrip`)

---
+++ lib/features/trip/domain/repositories/trip_repository.dart
@@ -0,0 +1,9 @@
+import '../entities/trip.dart';
+
+abstract class TripRepository {
+  Future<List<Trip>> getTrips();
+
+  Future<void> addTrip(Trip trip);
+
+  Future<void> deleteTrip(int index);
+}
