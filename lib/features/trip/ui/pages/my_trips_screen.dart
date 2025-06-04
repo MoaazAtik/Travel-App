77c9688cff7ea54b5ceeb4fa0b4dbf8f7f98bc2c
MoaazAtik <moaaz.atik@gmail.com>
2025-06-04 16:15:51 +0300

    feat: Add CustomSearchBar and integrate into MyTripsScreen without functionality yet
    This commit introduces a new `CustomSearchBar` widget.
    
    Currently, the `CustomSearchBar` is a styled `TextField` with a search icon, designed for destination searching. It's styled with a white background, rounded corners, and a subtle shadow.
    
    This search bar has been added to the `MyTripsScreen`. The screen's layout is now wrapped in a `SingleChildScrollView` to accommodate the search bar above the list of travel cards. The `ListView.builder` has been configured with `shrinkWrap: true` and `NeverScrollableScrollPhysics()` to ensure proper scrolling behavior within the `SingleChildScrollView`.

--- lib/features/trip/ui/pages/my_trips_screen.dart
+++ lib/features/trip/ui/pages/my_trips_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/trip_provider.dart';
+import '../widgets/custom_search_bar.dart';
 import '../widgets/travel_card.dart';
 
 class MyTripsScreen extends ConsumerWidget {
@@ -12,21 +13,31 @@ class MyTripsScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final tripList = ref.watch(tripListNotifierProvider);
 
-    return ListView.builder(
-      itemCount: tripList.length,
-      itemBuilder: (context, index) {
-        final trip = tripList[index];
-        return TravelCard(
-          imageUrl: trip.photos[0],
-          title: trip.title,
-          description: trip.description,
-          date: DateFormat.yMMMMd().format(trip.date),
-          location: trip.location,
-          onDelete: () {
-            ref.read(tripListNotifierProvider.notifier).removeTrip(index);
-          },
-        );
-      },
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(), // for mobile
+      child: Column(
+        children: [
+          CustomSearchBar(),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(), // necessary for mobile
+            itemCount: tripList.length,
+            itemBuilder: (context, index) {
+              final trip = tripList[index];
+              return TravelCard(
+                imageUrl: trip.photos[0],
+                title: trip.title,
+                description: trip.description,
+                date: DateFormat.yMMMMd().format(trip.date),
+                location: trip.location,
+                onDelete: () {
+                  ref.read(tripListNotifierProvider.notifier).removeTrip(index);
+                },
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
